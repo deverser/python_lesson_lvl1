04c4eeb05b12df58cc5fdbade2e517d2c332d7a1
Alexander <alexmal74@gmail.com>
2018-10-30 14:42:56 +0500

    Задача hard почти готова. нужно исправить некоторые ошибки

--- lesson3/les3_hw_hard_my.py
+++ lesson3/les3_hw_hard_my.py
@@ -16,36 +16,65 @@ enemy = {'name': None, 'health': 200, 'damage': 15}
 def give_name(person, name=None):
     '''Даем имена героям игры и записываем их в словари'''
     name = input('Введите имя игрока: ')
-    person['name'] = name
+    person['name'] = name.title()
     return person
 
 
-def fighting(player1, player2):
+def attack(player1, player2):
     '''Битва персонажей'''
-    name1 = player1['name'].title()
-    name2 = player2['name'].title()
+    fight = []
+    name1 = player1['name']
+    name2 = player2['name']
     damage = player1['damage']
-    damage2 = player2['damage']
-    health1 = player1['health']
     health = player2['health']
     while health > 0:
         health -= damage
         if health <=0:
             health = 0
-            print(
-                '{} attacks {} with {}. {}\'s xp is {}. {} wins the fight!'.format(
-                    name1, name2, damage, name2, health, name1))
+            msg = '{} attacks {} with {}. {}\'s xp is {}. {} wins the fight!'.format(
+                    name1, name2, damage, name2, health, name1)
+            fight.append(msg)
             break
-        print('{} attacks {} with {} dmg. {}\'s xp is {}'.format(
-            name1, name2, damage, name2, health))
+            return fight
+        msg = '{} attacks {} with {} dmg. {}\'s xp is {}'.format(
+            name1, name2, damage, name2, health)
+        fight.append(msg)
+    return fight
 
-##def epic_fight(attack1,attack2):
+def battle(attack1,attack2):
+    if len(attack1) > len(attack2):
+##        delta = len(attack1) - len(attack2)
+##        attack1 = attack1[:-delta]
+        print(len(attack1))
+        i = 0
+        while i < len(attack2):
+            print(attack1[i])
+            print(attack2[i])
+            i += 1
+    else:
+##        delta = len(attack2) - len(attack1)
+##        attack2 = attack2[:-delta]
+        print(len(attack2))
+        i = 0
+        while i < len(attack1):
+            print(attack1[i])
+            print(attack2[i])
+            i += 1
+        
     
 give_name(hero)
 give_name(enemy)
 
-fighting(hero,enemy)
-fighting(enemy,hero)
+hero_attack = attack(hero,enemy)
+enemy_attack = attack(enemy,hero)
+
+print(len(hero_attack))
+print(len(enemy_attack))
+
+battle(hero_attack, enemy_attack)
+
+
+
 # Задание - 2
 # Давайте усложним предыдущее задание, измените сущности, добавив новый параметр - armor = 1.2
 # Теперь надо добавить функцию, которая будет вычислять и возвращать полученный урон по формуле damage / armor
